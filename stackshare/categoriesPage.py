2c383563cd60bbea955b55a0d9889c08b7c58cbd
haiyesensi <github.com/haiyesensi>
2017-12-08 11:24:26 +0800

    获取bootstrap的简介

--- stackshare/categoriesPage.py
+++ stackshare/categoriesPage.py
@@ -1,3 +1,4 @@
+#! -*-utf-8-*-
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -5,42 +6,71 @@ import re
 DOMIN = 'https://stackshare.io'
 
 
-def get_categories(category=None):
+# def get_categories(category=None):
+#
+#     if category is None:
+#         category = '/categories'
+#
+#     categories_page = requests.get(DOMIN + category)
+#
+#     html = categories_page.text
+#
+#     soup = BeautifulSoup(html, "lxml")
+#
+#     li = soup.find('li', 'hide-4')
+#
+#     div = li.find('div', 'navmenu')
+#
+#     res = []
+#     for url in div.find_all('a'):
+#         res.append(url['href'])
+#     return res
+#
+#
+# def get_apps_ids(url_category=None):
+#     if url_category is None:
+#         return []
+#     category_html = requests.get(DOMIN + url_category).text
+#
+#     soup = BeautifulSoup(category_html, 'lxml')
+#
+#     all_script = soup.find_all(re.compile('^script'))
+#
+#     ids_script = re.search(r'ordered_service_ids.*]', str(all_script), re.M).group()  # ordered_service_ids = [...]
+#
+#     ids = re.sub('.*=', "", ids_script)
+#
+#     return eval(ids)
+#
+# res = get_apps_ids('/application_and_data')
+# print(res)
+# print(len(res))
 
-    if category is None:
-        category = '/categories'
 
-    categories_page = requests.get(DOMIN + category)
+def gettooldetail(toolname = None):
 
-    html = categories_page.text
+    toolpage = requests.get(DOMIN +toolname).text
 
-    soup = BeautifulSoup(html, "lxml")
+    soup = BeautifulSoup(toolpage,"lxml")
+    #print(soup.prettify())
 
-    li = soup.find('li', 'hide-4')
-
-    div = li.find('div', 'navmenu')
-
-    res = []
-    for url in div.find_all('a'):
-        res.append(url['href'])
-    return res
+    # 需要获取的信息
+    # 1.name,2.explain,3.son_label,4.second_label,5.papa_label,6.star_count,7.votes_count,8.fans_count,9.company using it
 
 
-def get_apps_ids(url_category=None):
-    if url_category is None:
-        return []
-    category_html = requests.get(DOMIN + url_category).text
+    #get explain imformation
+    link = soup.find_all('span')
+    explain = re.search(r'.*alternativeHeadline.*<',str(link),re.M).group()
+    explain = re.sub(r'.*=', "", explain)
+    explain = re.sub(r'<.*',"",explain,2)
+    explain = re.sub(r'.*>',"",explain)
+    print(explain)
 
-    soup = BeautifulSoup(category_html, 'lxml')
+    #get son_label
+    #son_label = re.search(r'.*name.*>',str(link),re.M).group()
+    #print(son_label)
 
-    all_script = soup.find_all(re.compile('^script'))
+res = gettooldetail('/bootstrap')
 
-    ids_script = re.search(r'ordered_service_ids.*]', str(all_script), re.M).group()  # ordered_service_ids = [...]
-
-    ids = re.sub(r'.*=', "", ids_script)
-
-    return eval(ids)
-
-print(get_apps_ids('/application_and_data'))
 
 
