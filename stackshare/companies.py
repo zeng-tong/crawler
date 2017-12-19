5e563924786c0d8608608965105989a239f741c0
zengtong <zengtong@zhihu.com>
2017-12-19 13:38:11 +0800

    公司信息爬取引入 redis ,避免重复抓取.

--- stackshare/companies.py
+++ stackshare/companies.py
@@ -1,20 +1,33 @@
 # -*- utf-8 -*-
 import requests
-from config import GetLogger
+from config import GetLogger, PyRedis
 from stackshare.src import get_item, constants, companies_info
 from config import mysql_session
+from stackshare.src.constants import CATEGORY_KEY
+from stackshare.src.utils import prepareCategories
 
 logger = GetLogger('companies').get_logger()
 
 
-def start():
-    categories = get_item.get_categories()
-    for each in categories:
-        if each == '/trending/new':
-            continue
-        items = get_item.get_item(requests.get(constants.DOMAIN + each))
+class Company:
+    def __init__(self):
+        self.__redis = PyRedis().get_resource()
+
+    def start(self):
+        category = self.__redis.spop(CATEGORY_KEY)
+        while category:
+            category = str(category, encoding='utf-8')
+            if category == '/trending/new':
+                category = self.__redis.spop(CATEGORY_KEY)
+                continue
+            self.save(category)
+            category = self.__redis.spop(CATEGORY_KEY)
+        logger.info(msg='Process finished')
+
+    def save(self, category):
+        items = get_item.get_item(requests.get(constants.DOMAIN + category))
         for item in items:
-            result = companies_info.companies_info(item['url']).companies()
+            result = companies_info.CompaniesInfo(item['url']).companies()
             companies = next(result)
             while companies:
                 for entity in companies:
@@ -23,16 +36,17 @@ def start():
                         session.expunge_all()
                         session.add(entity)
                         session.commit()
-                        logger.info(msg='Company 「' + entity.company_name + '」 save to mysql succeed...')
+                        logger.info(msg='Company 「' + entity.company_name + '」under ' + item['name'] + ' save to mysql succeed...')
                     except Exception as e:
                         print(e)
                     finally:
                         session.close()
                 companies = next(result)
-    logger.info(msg='Process finished')
+
 
 if __name__ == '__main__':
-    start()
+    prepareCategories()
+    Company().start()
 
 
 
--- stackshare/src/companies_info.py
+++ stackshare/src/companies_info.py
@@ -1,15 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+from config import PyRedis
 from stackshare.models.companies import Companies
 from stackshare.src import constants
 from stackshare.src.exceptions import InvalidValueException
 
 
-class companies_info:
+class CompaniesInfo:
 
     def __init__(self, app_url):
         self.__app_url = app_url
+        self.redis = PyRedis().get_resource()
 
     def companies(self):
         result = self.__companies()
@@ -23,8 +25,7 @@ class companies_info:
 
     def __companies(self):
         companies = []
-        headers = {
-            'Cookie': 'ajs_anonymous_id=%22a1aad3a2-0df8-41f8-9ca0-b61008d9f68d%22; __uvt=; __atuvc=2%7C48; intercom-id-rsodlit1=bad16bb0-0d8a-4de1-b7d0-4412cb277075; wooTracker=bhMtogmHMTUg; fs_uid=www.fullstory.com`1WAJR`6330646235447296:5681097123823616`222312`; ajs_group_id=null; _ga=GA1.2.1999935515.1512042170; _gid=GA1.2.1540975249.1512647730; ajs_user_id=222312; amplitude_idstackshare.io=eyJkZXZpY2VJZCI6IjQ1OGU1NDk2LWU5ODctNDBjNi04MDQxLWIyZDZmMjkwYTljY1IiLCJ1c2VySWQiOiIyMjIzMTIiLCJvcHRPdXQiOmZhbHNlLCJzZXNzaW9uSWQiOjE1MTI3MzkyNjkwODQsImxhc3RFdmVudFRpbWUiOjE1MTI3NDI1MDY2NzYsImV2ZW50SWQiOjE5NywiaWRlbnRpZnlJZCI6NjUsInNlcXVlbmNlTnVtYmVyIjoyNjJ9; uvts=6pNGSSEwGigBIJEJ; intercom-session-rsodlit1=N1VsRlVld2xsbURLWVZpMURITDMxNUM2NkJoY3VBOGhsMTJEVkJhMTgxWlpjSmpYUDdrNGhwTndHdkh3ZVRRTS0tVWQwcXY1eERkSFBYTCtKWWdRMVhtUT09--c34f5c040bafb8f6059848d8ecbcdf42d1e94ce7; intercom-lou-rsodlit1=1; XSRF-TOKEN=pzu7cLRmOTVWDF0WeyoEN8KFuOHQOMrVknF0gZP5RcDnLsphbDMuHVFEasWIpZ%2Fosd8jZt7j85MEqAslb84TAg%3D%3D; _stackshare_production_session=M0ppSHpZUFNnUzhWbnlyNjFOdHA1NTh4dnljcVc2V1BKVXFleHovVjU3QitvQjJIbzI1cFZrcXZsZklYb3RXYkZPR21SZk93dlp1MHJXR080TUJLb0pNSTRpdmxoam1xTlVNcjlJeER1L1ZCQWJNalFMYnpDcFU2MHNXS3ZsQW5TUUZhZnAxalJXVitjK3lDUmdVMGZtNXNOWDkxczBwSzBQeFhyMWtRTjQ5MlllRUN1UnFWVlIvUFl3LzgvdE84dWcrNHRtMDBlQTdWVWFob3Yyd1FUQ0VQc3pzVVB1NFRISWd4TnFVTktZZkFvRkFGRDh3ZmxjUXJGdUdxQnV4MENzakJsaGIzWFVENnlIODdRdG1jcmlJT3BNdWh1MURkdkZLVjdtZkRaODQ9LS1QZWt1Q1pxbnY5VHpWUmVmWHZod0xRPT0%3D--c99c145b46d7c59d1faa73cff4b74cabbfcee641; _gat=1'}
+        headers = {'Cookie': 'ajs_anonymous_id=%22a1aad3a2-0df8-41f8-9ca0-b61008d9f68d%22; __uvt=; __atuvc=2%7C48; intercom-id-rsodlit1=bad16bb0-0d8a-4de1-b7d0-4412cb277075; wooTracker=bhMtogmHMTUg; fs_uid=www.fullstory.com`1WAJR`6330646235447296:5681097123823616`222312`; ajs_group_id=null; _ga=GA1.2.1999935515.1512042170; _gid=GA1.2.1540975249.1512647730; ajs_user_id=222312; amplitude_idstackshare.io=eyJkZXZpY2VJZCI6IjQ1OGU1NDk2LWU5ODctNDBjNi04MDQxLWIyZDZmMjkwYTljY1IiLCJ1c2VySWQiOiIyMjIzMTIiLCJvcHRPdXQiOmZhbHNlLCJzZXNzaW9uSWQiOjE1MTI3MzkyNjkwODQsImxhc3RFdmVudFRpbWUiOjE1MTI3NDI1MDY2NzYsImV2ZW50SWQiOjE5NywiaWRlbnRpZnlJZCI6NjUsInNlcXVlbmNlTnVtYmVyIjoyNjJ9; uvts=6pNGSSEwGigBIJEJ; intercom-session-rsodlit1=N1VsRlVld2xsbURLWVZpMURITDMxNUM2NkJoY3VBOGhsMTJEVkJhMTgxWlpjSmpYUDdrNGhwTndHdkh3ZVRRTS0tVWQwcXY1eERkSFBYTCtKWWdRMVhtUT09--c34f5c040bafb8f6059848d8ecbcdf42d1e94ce7; intercom-lou-rsodlit1=1; XSRF-TOKEN=pzu7cLRmOTVWDF0WeyoEN8KFuOHQOMrVknF0gZP5RcDnLsphbDMuHVFEasWIpZ%2Fosd8jZt7j85MEqAslb84TAg%3D%3D; _stackshare_production_session=M0ppSHpZUFNnUzhWbnlyNjFOdHA1NTh4dnljcVc2V1BKVXFleHovVjU3QitvQjJIbzI1cFZrcXZsZklYb3RXYkZPR21SZk93dlp1MHJXR080TUJLb0pNSTRpdmxoam1xTlVNcjlJeER1L1ZCQWJNalFMYnpDcFU2MHNXS3ZsQW5TUUZhZnAxalJXVitjK3lDUmdVMGZtNXNOWDkxczBwSzBQeFhyMWtRTjQ5MlllRUN1UnFWVlIvUFl3LzgvdE84dWcrNHRtMDBlQTdWVWFob3Yyd1FUQ0VQc3pzVVB1NFRISWd4TnFVTktZZkFvRkFGRDh3ZmxjUXJGdUdxQnV4MENzakJsaGIzWFVENnlIODdRdG1jcmlJT3BNdWh1MURkdkZLVjdtZkRaODQ9LS1QZWt1Q1pxbnY5VHpWUmVmWHZod0xRPT0%3D--c99c145b46d7c59d1faa73cff4b74cabbfcee641; _gat=1'}
         # get company id
         response = requests.get(constants.DOMAIN + self.__app_url + '/in-stacks', headers=headers)
         company_soup = BeautifulSoup(response.text, 'lxml')
@@ -38,7 +39,12 @@ class companies_info:
         app_id = text['data-service-id']
 
         # request data page by page.
-        payload = {'page': 1, 'service_id': app_id}
+        page = self.redis.hget('companies_page', self.__app_url)
+
+        # if not exist means it's the first time to run the category.
+        page = int(page) if page else 1
+        payload = {'page': page, 'service_id': app_id}
+
         url = constants.DOMAIN + '/service-stacks-load-more'
         page = requests.post(url, data=payload)
         while page.text:
@@ -46,7 +52,10 @@ class companies_info:
             for each in detail_soup.find_all('div', 'col-md-1 stack-logo'):
                 companies.append({'company': each.find('img')['alt'], 'logo': each.find('img')['src']})
             yield companies
+
             # 避免下次重复插入
             companies = []
+
             payload['page'] = payload['page'] + 1
+            self.redis.hset('companies_page', self.__app_url, payload['page'])
             page = requests.post(url, data=payload)
